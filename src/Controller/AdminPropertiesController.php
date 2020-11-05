fcadddce7d12cfe61d2803fe27792c790457f8d6
SYLVESTRE Samuel <s.sylvestre@hotmail.com>
2020-11-05 12:50:29 +0100

    Commit creation admin add edit cancel un bien et regle de validation champs

--- src/Controller/AdminPropertiesController.php
+++ src/Controller/AdminPropertiesController.php
@@ -32,6 +32,7 @@ class AdminPropertiesController extends AbstractController
      * @Route ("/admin/property/new", name="admin_property_new")
      * @param PropertyRepository $propertyRepository
      * @param Request $request
+     * @param PropertyType $form
      * @return Response
      */
     public function new(PropertyRepository $propertyRepository, Request $request, PropertyType $form)
@@ -39,34 +40,31 @@ class AdminPropertiesController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $property = new Property();
 
-        $form = $this->createForm(PropertyType::class);
+        $form = $this->createForm(PropertyType::class, $property);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid())
         {
             $em->persist($property);
             $em->flush();
+            $this->addFlash('success','Bien créé avec succès !');
             return new Response($this->redirectToRoute('admin'));
 
         }
         return $this->render('admin/properties/new.html.twig',[
-
+            'property'=> $property,
+            'form' => $form->createView()
         ]);
-
-
-
-
     }
 
 
     /**
-     * @Route("/admin/property/{id}", name="admin_property_edit")
+     * @Route("/admin/property/{id}", name="admin_property_edit", methods={"GET", "POST"})
      * @param PropertyRepository $propertyRepository
      * @param Property $property
      * @param int $id
      * @param PropertyType $form
      * @param Request $request
-     * @param EntityManagerInterface $em
      * @return Response
      */
     public function edit(PropertyRepository $propertyRepository, Property $property, int $id, PropertyType $form, Request $request): Response
@@ -80,6 +78,7 @@ class AdminPropertiesController extends AbstractController
         if ($form->isSubmitted() && $form->isValid())
         {
             $em->flush();
+            $this->addFlash('success','Bien modifié avec succès !');
             return new Response($this->redirectToRoute('admin'));
 
         }
@@ -88,6 +87,26 @@ class AdminPropertiesController extends AbstractController
             'property'=> $property,
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/admin/property/{id}", name="admin_property_delete", methods={"DELETE"})
+     * @param PropertyRepository $propertyRepository
+     * @param Property $property
+     * @param Request $request
+     * @return Response
+     */
+    public function delete (PropertyRepository $propertyRepository, Property $property, Request $request): Response
+    {
+
+        if($this->isCsrfTokenValid('delete', $request->get('_token')))
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($property);
+            $em->flush();
+            $this->addFlash('success','Bien supprimé avec succès !');
+            return $this->redirectToRoute('admin');
+        }
     }
 
 }
--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=PropertyRepository::class)
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -25,6 +28,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="5", max="40")
      */
     private $title;
 
@@ -36,6 +40,7 @@ class Property
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\Range(min="10", max="400")
      */
     private $surface;
 
@@ -76,6 +81,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[0-9]{5}$/")
      */
     private $postal_code;
 
